3c10727c95386ebfa310eb4284442e80cf238d18
kg6415552-code <kg6415552@gmail.com>
2026-04-21 17:08:31 +0800

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// JOIN NOW BUTTON ACTION
 function joinNow() {
-alert("Welcome to JetSet Fitness Studio! Let's start your fitness journey!");
+  alert("Welcome to JetSet Fitness Studio! 💪\nPlease contact us or visit the gym to join.");
 }
